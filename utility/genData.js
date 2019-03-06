9090a75fe32b019e88857d3d95f3fe93b92da7af
formulahendry <formulahendry@gmail.com>
2019-03-07 00:17:47 +0800

    Generate data for activity

---
+++ utility/genData.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const dummyjson = require('dummy-json');
+
+async function genData(count) {
+    // Connection URL
+    const url = 'mongodb://*****';
+
+    // Database Name
+    const dbName = 'test';
+
+    // Use connect method to connect to the server
+    const client = await MongoClient.connect(url);
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+
+    let collection = db.collection('activity');
+    var template = fs.readFileSync('./data/activity.hbs', {encoding: 'utf8'});
+    for (let i = 0; i < count; i++) {
+        const result = dummyjson.parse(template);
+        await collection.insertOne(JSON.parse(result));
+        console.log("Iteration #"+i);
+        console.log(JSON.parse(result));
+    }
+
+    client.close();
+}
+
+genData(3);
+
+
